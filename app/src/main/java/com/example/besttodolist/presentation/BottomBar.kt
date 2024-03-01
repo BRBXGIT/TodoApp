ae1112031d771bd1bf8fad8f6f4e6cf6c144739a
BRBX <egorka2005sautkin@gmail.com>
2024-03-02 01:01:26 +0300

    create navigation bar and chage main screen a litle bit

---
+++ app/src/main/java/com/example/besttodolist/presentation/BottomBar.kt
@@ -0,0 +1,64 @@
+package com.example.besttodolist.presentation
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.besttodolist.R
+import com.example.besttodolist.presentation.main_screen.MainScreenViewModel
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun BottomBar() {
+
+    val mainScreenViewModel = hiltViewModel<MainScreenViewModel>()
+
+    BottomAppBar(
+        containerColor = Color(0xff223148),
+        modifier = Modifier
+            .height(60.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Icon(painter = painterResource(id = R.drawable.ic_home_outlined), contentDescription = "Home icon")
+
+            FloatingActionButton(
+                onClick = { mainScreenViewModel.upsertTodo("YES", "02-03-24") },
+                contentColor = Color(0xfff7f7f7),
+                shape = RoundedCornerShape(10.dp),
+                containerColor = Color(0xff85aff7),
+                modifier = Modifier
+                    .size(40.dp)
+            ) {
+                Icon(painter = painterResource(id = R.drawable.ic_plus), contentDescription = "Plus icon")
+            }
+
+            Icon(painter = painterResource(id = R.drawable.ic_calendar_outlined), contentDescription = "Calendar icon")
+        }
+    }
+}
--- app/src/main/java/com/example/besttodolist/presentation/main_screen/MainScreen.kt
+++ app/src/main/java/com/example/besttodolist/presentation/main_screen/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.example.besttodolist.presentation.main_screen
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,17 +14,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
@@ -35,125 +45,134 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.example.besttodolist.R
+import com.example.besttodolist.presentation.BottomBar
 import com.example.besttodolist.presentation.sign_in.UserData
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen(
     userData: UserData?,
     onSignOut: () -> Unit
 ) {
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setNavigationBarColor(Color(0xff223148))
+
     val fontForLogo = FontFamily(Font(R.font.protestriot_regular))
     val mainScreenViewModel = hiltViewModel<MainScreenViewModel>()
 
     val formatter = DateTimeFormatter.ofPattern("dd-MM-yy")
     val currentDate = LocalDateTime.now().format(formatter)
 
-    //Main column
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xff162232))
+    Scaffold(
+        bottomBar = { BottomBar() },
+        floatingActionButtonPosition = FabPosition.Center
     ) {
-
-        Spacer(modifier = Modifier.height(15.dp))
-
-        Row(
+        //Main column
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.05f)
-                .padding(start = 16.dp, end = 32.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+                .fillMaxSize()
+                .background(Color(0xff162232))
         ) {
-            Text(
-                text = "Todo List",
-                fontWeight = FontWeight.Bold,
-                fontFamily = fontForLogo,
-                fontSize = 20.sp,
-                color = Color(0xff85aff7),
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            Row(
                 modifier = Modifier
-                    .clickable { mainScreenViewModel.upsertTodo("Yes yes yes", currentDate) }
-            )
-            if(userData?.profilePictureUrl != null) {
-                AsyncImage(
-                    model = userData.profilePictureUrl,
-                    contentDescription = "Profile picture",
-                    modifier = Modifier
-                        .size(32.dp)
-                        .clip(CircleShape)
-                        .clickable {
-                            onSignOut()
-                        },
-                    contentScale = ContentScale.Crop
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.05f)
+                    .padding(start = 16.dp, end = 32.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Todo List",
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = fontForLogo,
+                    fontSize = 20.sp,
+                    color = Color(0xff85aff7),
+                )
+                if(userData?.profilePictureUrl != null) {
+                    AsyncImage(
+                        model = userData.profilePictureUrl,
+                        contentDescription = "Profile picture",
+                        modifier = Modifier
+                            .size(32.dp)
+                            .clip(CircleShape)
+                            .clickable {
+                                onSignOut()
+                            },
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.05f)
+                    .padding(start = 16.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Text(
+                    buildAnnotatedString {
+                        withStyle(SpanStyle(color = Color(0xfff7f7f7), fontSize = 18.sp)) {
+                            append("Today  ")
+                        }
+                        withStyle(SpanStyle(color = Color(0xff787b7b), fontSize = 17.sp, fontWeight = FontWeight.Thin)) {
+                            append(currentDate)
+                        }
+                    }
                 )
             }
-        }
 
-        Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(10.dp))
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.05f)
-                .padding(start = 16.dp),
-            contentAlignment = Alignment.CenterStart
-        ) {
-            Text(
-                buildAnnotatedString {
-                    withStyle(SpanStyle(color = Color(0xfff7f7f7), fontSize = 18.sp)) {
-                        append("Today  ")
-                    }
-                    withStyle(SpanStyle(color = Color(0xff787b7b), fontSize = 17.sp, fontWeight = FontWeight.Thin)) {
-                        append(currentDate)
-                    }
-                }
+            val unCompletedTodos = mainScreenViewModel.getUncompletedTodosByDate(currentDate).collectAsState(
+                initial = emptyList()
             )
-        }
+            val completedTodos = mainScreenViewModel.getCompletedTodosByDate(currentDate).collectAsState(
+                initial = emptyList()
+            )
 
-        Spacer(modifier = Modifier.height(10.dp))
-
-        val unCompletedTodos = mainScreenViewModel.getUncompletedTodosByDate(currentDate).collectAsState(
-            initial = emptyList()
-        )
-        val completedTodos = mainScreenViewModel.getCompletedTodosByDate(currentDate).collectAsState(
-            initial = emptyList()
-        )
-
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxHeight(0.4f)
-                .fillMaxWidth()
-                .padding(start = 32.dp, end = 32.dp)
-        ) {
-            items(unCompletedTodos.value.sortedByDescending { todo -> todo.isInBookmark }, key = {todo -> todo.id}) { todo ->
-                TodoItem(id = todo.id, title = todo.title, date = todo.date, isInBookmark = todo.isInBookmark, isCompleted = todo.isCompleted)
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxHeight(0.4f)
+                    .fillMaxWidth()
+                    .padding(start = 32.dp, end = 32.dp)
+            ) {
+                items(unCompletedTodos.value.sortedByDescending { todo -> todo.isInBookmark }, key = {todo -> todo.id}) { todo ->
+                    TodoItem(id = todo.id, title = todo.title, date = todo.date, isInBookmark = todo.isInBookmark, isCompleted = todo.isCompleted)
+                }
             }
-        }
 
-        Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(10.dp))
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.07f)
-                .padding(start = 16.dp),
-            contentAlignment = Alignment.CenterStart
-        ) {
-            Text(text = "Completed Today", color = Color(0xfff7f7f7), fontSize = 18.sp)
-        }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.07f)
+                    .padding(start = 16.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Text(text = "Completed Today", color = Color(0xfff7f7f7), fontSize = 18.sp)
+            }
 
-        Spacer(modifier = Modifier.height(15.dp))
+            Spacer(modifier = Modifier.height(15.dp))
 
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.8f)
-                .padding(start = 32.dp, end = 32.dp)
-        ) {
-            items(completedTodos.value,  key = {todo -> todo.id}) { todo ->
-                CompletedTodoItem(title = todo.title, date = todo.date, id = todo.id, isInBookmark = todo.isInBookmark, isCompleted = todo.isCompleted)
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.821f)
+                    .padding(start = 32.dp, end = 32.dp)
+            ) {
+                items(completedTodos.value,  key = {todo -> todo.id}) { todo ->
+                    CompletedTodoItem(title = todo.title, date = todo.date, id = todo.id, isInBookmark = todo.isInBookmark, isCompleted = todo.isCompleted)
+                }
             }
         }
     }
